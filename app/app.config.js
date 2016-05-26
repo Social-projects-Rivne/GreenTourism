8d123b1d95e165bf9532226dbf2574dfe4e2ac9c
yurykozhenov <yurykozhenov@gmail.com>
2016-05-26 16:14:50 +0300

    Separate application configuration

---
+++ app/app.config.js
@@ -0,0 +1,12 @@
+app.config(function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: '<welcome-page></welcome-page>',
+    }).
+    when('/places', {
+      templateUrl: '<place-list></place-list>',
+    }).
+    when('/places/:placeId', {
+      templateUrl: '<place-detail></place-detail>'
+    });
+});
