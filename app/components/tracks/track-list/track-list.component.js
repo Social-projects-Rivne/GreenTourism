8d5d0995e38b7a54f23d87bfea112d39e09f613f
Denys Gudakov <denysgud@gmail.com>
2016-06-06 19:39:04 +0300

    Add tracks filter

--- app/components/tracks/track-list/track-list.component.js
+++ app/components/tracks/track-list/track-list.component.js
@@ -1,5 +1,45 @@
 angular.module('trackList', [])
 .component('trackList', {
     templateUrl: 'components/tracks/track-list/track-list.template.html',
-    controller: function() { }
+    controller: ['$http',  function trackListController($http) {
+        var ctrlScope = this;
+        var tracks = [];
+        ctrlScope.walkingActive = true;
+        ctrlScope.carActive = true;
+        ctrlScope.bycicleActive = true;
+        ctrlScope.mixedActive = true;
+        
+        $http.get("components/tracks/track-list/tracks.data.json").success(function (data) {
+            ctrlScope.tracks = data;
+            var mymap = L.map('map').setView([50.61734, 26.25217], 13);         
+                            
+            L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a rel="nofollow" href="http://osm.org/copyright">OpenStreetMap</a> contributors | GreenTourism'
+            }).addTo(mymap);
+            
+            for (var i = 0; i < ctrlScope.tracks.length; i++) {
+                var polyline = L.polyline([ctrlScope.tracks[i].track_points], {color: ctrlScope.tracks[i].color}).addTo(mymap);
+                tracks.push([polyline, ctrlScope.tracks[i].type]);
+            }
+
+            // zoom the map to the polyline
+            //mymap.fitBounds(polyline.getBounds());
+            ctrlScope.showSpecificTracks = function (inputValue, type) {
+                for (var i = 0; i < tracks.length; i++) {
+                    if (inputValue) {                   
+                        if (type == ctrlScope.tracks[i].type) {
+                            tracks[i][0].addTo(mymap);
+                        } 
+                    } else {
+                        if (type == ctrlScope.tracks[i].type) {
+                            mymap.removeLayer(tracks[i][0]);
+                        }
+                    }
+                }
+            };
+            console.log(tracks);
+        }).error(function (data) {
+            ctrlScope.tracks = "error";
+        });
+    }]
   });
