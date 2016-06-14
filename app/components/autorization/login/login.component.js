c818a87ba36484283fb12d9d38cd91970783993e
TarasRudyk <rudyktaras@gmail.com>
2016-06-14 18:14:55 +0300

    signin with json file

--- app/components/autorization/login/login.component.js
+++ app/components/autorization/login/login.component.js
@@ -1,45 +1,22 @@
-'use strict';
-
+//'use strict';
 angular.module('login', [])
 .component('login', {
     templateUrl: 'components/autorization/login/login.template.html',
-    controller: function msgCtrl() {
-        this.submit=function (userDetails) {
-            this.name = userDetails.name;
-            this.password = userDetails.password;
-
-            var modeluser = [{
-                "username": "admin",
-                "email": "rudyktaras@gmail.com",
-                "password": "admin",
-                },{
-                "username": "taras",
-                "email": "rudyktaras@gmail.com",
-                "password": "taras",
-                }]
-
-
-           if (this.name == "admin"  && this.password == "admin" ){
-               window.location.hash = "/places"
-           }else{
-               this.message = "no such user here";
-           }
-       };
+    controller: function msgCtrl($http) {
+        $http.get('components/autorization/users.json').then(function(response) {
+             users = response.data;
+        });
+            this.submit=function (userDetails) {
+                this.email = userDetails.email;
+                this.password = userDetails.password;
+                for (var i = 0; i < users.length; i++){
+                    var user = users[i];
+                        if (this.email == user.email && this.password == user.password){
+                            window.location.hash = "/profile"
+                        }else{
+                            this.message = "No such user here";
+                        };
+                };
+            };
    },
   });
-
-
-  // $http.get('components/autorization/users.json').then(function(response) {
-  //   users.email = response.data;
-  //   users.password = response.data;
-  // });
-
-  // var arr = [ {"id":"10", "class": "child-of-9"}, {"id":"11", "classd": "child-of-10"}];
-  //
-  //    for(var i=0;i<arr.length;i++){
-  //        var obj = arr[i];
-  //        for(var key in obj){
-  //            var attrName = key;
-  //            var attrValue = obj[key];
-  //        }
-  //    }
