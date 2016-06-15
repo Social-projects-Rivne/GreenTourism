379a765e7946d420d0c6a4cd3ce40c1d27d7899e
Yury Kozhenov <yurykozhenov@gmail.com>
2016-06-15 16:07:01 +0300

    Add tab switching

--- app/components/user-profile/user-profile.component.js
+++ app/components/user-profile/user-profile.component.js
@@ -3,14 +3,27 @@ angular.module('userProfile', []);
 angular.module('userProfile').component('userProfile', {
   templateUrl: 'components/user-profile/user-profile.template.html',
   controller: function() {
-    this.user = {
-      firstName: 'John',
-      lastName: 'Smith',
-      nickname: 'johnsmith',
-      avatar: 'assets/img/user-profile/avatar.png',
-      getFullName: function() {
-        return this.firstName + ' ' + this.lastName;
-      }
+    this.user = new User('John', 'Smith', 'johnsmith',
+                         'assets/img/user-profile/avatar.png');
+
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab == checkTab;
     };
   }
 });
+
+function User(firstName, lastName, nickname, avatar) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.nickname = nickname;
+  this.avatar = avatar;
+  this.getFullName = function() {
+    return this.firstName + ' ' + this.lastName;
+  };
+}
