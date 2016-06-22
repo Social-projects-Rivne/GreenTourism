12943686f2a432ecfc0bd34db3356deba23a013d
SergiiSemenkiv <ssv260689@gmail.com>
2016-06-22 12:44:16 +0300

    added all data blogs, and comment addings in blog component

--- app/components/blog/blog-detail/blog-detail.component.js
+++ app/components/blog/blog-detail/blog-detail.component.js
@@ -6,7 +6,12 @@ angular.module('blog').component('blogDetail', {
             var self = this;
             self.orderProp = 'date';
             $http.get('components/blog/blog.data.json').then(function (response) {
-                self.blogs = $filter('filter')(response.data, {id: $routeParams.blogId})
+                self.blog = $filter('filter')(response.data, {id: $routeParams.blogId});
+                self.blogs = $filter('filter')(response.data, {id: '!' + $routeParams.blogId});
             });
+            self.newComment = {};
+            self.addComment = function(blog){
+                blog.comments.push(self.newComment);
+       	    }
         }
 });
