b35628391955d521938f3afdc685d07a594a699f
SergiiSemenkiv <ssv260689@gmail.com>
2016-06-22 12:34:05 +0300

    move blog category to blog-list.component

--- app/components/blog/blog-list/blog-list.component.js
+++ app/components/blog/blog-list/blog-list.component.js
@@ -11,3 +11,23 @@ angular.module('blogList').component('blogList', {
         });
     }
 });
+angular.module('blogCategory', [])
+    .filter("unique", function () {
+        return function (data, propertyName) {
+            if (angular.isArray(data) && angular.isString(propertyName)) {
+                var results = [];
+                var keys = {};
+                for (var i = 0; i < data.length; i++) {
+                    var val = data[i][propertyName];
+                    if (angular.isUndefined(keys[val])) {
+                        keys[val] = true;
+                        results.push(val);
+                    }
+                }
+                return results;
+            } else {
+                return data;
+            }
+        }
+
+    });
