2a96891b8287968158fa5f353ff5d9472880ac96
Yury Kozhenov <yurykozhenov@gmail.com>
2016-06-27 06:30:03 +0300

    Add image and template minification, remove unnecessary comments

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,13 @@ var useref = require('gulp-useref');
 var gulpif = require('gulp-if');
 var minifyCss = require('gulp-clean-css');
 var clean = require('gulp-clean');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
 //var templateCache = require('gulp-angular-templatecache');
-//var htmlmin = require('gulp-htmlmin');
 
-var DEST = 'dist/';
+var DEST = 'dist/';  // Destination folder
 
+// Compile less files
 gulp.task('less', function() {
   return gulp.src('app/assets/less/main.less')
     .pipe(plumber(function(err) {
@@ -25,6 +27,7 @@ gulp.task('less', function() {
     .pipe(notify('Less compiled!'));
 });
 
+// Add bower deps to index.html
 gulp.task('bower', function() {
   return gulp.src('app/index.html')
     .pipe(wiredep({
@@ -34,34 +37,41 @@ gulp.task('bower', function() {
     .pipe(notify('Bower dependencies injected!'));
 });
 
+// Clean destinaton folder
 gulp.task('clean', function() {
   return gulp.src(DEST, {read: false})
     .pipe(clean())
     .pipe(notify('Destination folder cleared!'));
 });
 
+// Build index.html, css, js
 gulp.task('build-assets', ['bower', 'less'], function() {
   return gulp.src('app/*.html')
       .pipe(useref())
-      .pipe(gulpif('*.js', uglify({mangle: false})))
+      .pipe(gulpif('*.js', uglify({mangle: false}))) // Consider using Google Closure Compiler instead
       .pipe(gulpif('*.css', minifyCss()))
       .pipe(gulp.dest(DEST))
       .pipe(notify('Assets builded!'));
 });
 
+// Copy all images and minify them
 gulp.task('copy-images', function() {
   return gulp.src('app/assets/img/**/*', {base: 'app'})
+    .pipe(imagemin())
     .pipe(gulp.dest(DEST))
     .pipe(notify('Images copied!'));
 });
 
+// Copy all angular templates and minify them
 gulp.task('copy-templates', function() {
   return gulp.src(['app/components/**/*.html', 'app/shared/**/*.html'],
                   {base: 'app'})
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest(DEST))
     .pipe(notify('Templates copied!'));
 });
 
+// Copy all JSON fixtures
 gulp.task('copy-JSON', function() {
   return gulp.src(['app/components/**/*.json', 'app/shared/**/*.json'],
                    {base: 'app'})
@@ -69,56 +79,18 @@ gulp.task('copy-JSON', function() {
     .pipe(notify('JSON files copied!'));
 });
 
+// Copy all necessary files
 gulp.task('build-copy', function(cb) {
   return runSequence('copy-images', 'copy-templates', 'copy-JSON', cb);
 });
 
+// Build project
 gulp.task('build', function(cb) {
   return runSequence('clean', 'build-copy', 'build-assets', cb);
 });
 
+// Automatically recompile less and index.html
 gulp.task('watch', function() {
   gulp.watch('app/assets/less/**/*.less', ['less']);
   gulp.watch('bower.json', ['bower']);
 });
-
-//gulp.task('js', function() {
-//  return gulp.src(['app/app.module.js', 'app/app.routes.js', 'app.models.js',
-//                   'app/shared/**/*.js', 'app/components/**/*.js'])
-//    .pipe(concat('app.js'))
-//    //.pipe(gulp.dest(DEST))
-//    //.pipe(uglify({mangle: false}))  // Consider using Google Closure Compiler instead
-//    //.pipe(rename({extname: '.min.js'}))
-//    .pipe(gulp.dest(DEST));
-//});
-
-//gulp.task('templates', function() {
-//  return gulp.src(['app/shared/**/*.html', 'app/components/**/*.html'])
-//    .pipe(templateCache())
-//    .pipe(gulp.dest(DEST));
-//});
-
-//gulp.task('html', ['js', 'templates'], function() {
-//  return gulp.src('app/index.html')
-//    .pipe(inject(gulp.src(['dist/app.js', 'dist/templates.js'],
-//                          {read: false}), {relateive: true}))
-//    .pipe(gulp.dest(DEST))
-//    .pipe(htmlmin({collapseWhitespace: true}))
-//    .pipe(rename({extname: '.min.html'}))
-//    .pipe(gulp.dest(DEST));
-//});
-
-//gulp.task('bower', function() {
-//  return gulp.src('app/bower_components/**')
-//    .pipe(gulp.dest(DEST + 'bower_components/'));
-//});
-
-//gulp.task('libs', function() {
-//  return gulp.src('app/libs/**')
-//    .pipe(gulp.dest(DEST + '/libs'));
-//});
-
-//gulp.task('json', function() {
-//  return gulp.src('app/**/*.json')
-//    .pipe(gulp.dest(DEST));
-//});
