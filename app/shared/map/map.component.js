44aa3cc676e3e5e6fda50539c3dc679657588af9
helenrivne <martyn4uk@gmail.com>
2016-07-04 23:37:11 +0300

    Update map.component.js

--- app/shared/map/map.component.js
+++ app/shared/map/map.component.js
@@ -1,36 +1,54 @@
 'use strict';
 
-angular.module('showMap', [])
-  .component('showMap', {
-    templateUrl: 'shared/map/map.template.html',
-    controller: function($rootScope) {
-      $rootScope.Outdoors = L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      });
-      $rootScope.Streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      });
-      $rootScope.Satellite = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-      });
+angular.module('mapModule', [])
+    .factory('mapFactory', function() {
+      var mapFactory = {};
+      var Streets;
+      var map;
+      mapFactory.showMap = function() {
+        Streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+         });
 
-      $rootScope.map = L.map('map', {
-        center: [50.6234, 26.2189],
-        zoom: 14
-      });
-      $rootScope.map.addLayer($rootScope.Streets);
-      angular.element("#Streets span").addClass('glyphicon glyphicon-ok');
+        map = L.map('map', {
+          center: [50.6234, 26.2189],
+          zoom: 14
+        });
+        mapFactory.map = map;
+        map.addLayer(Streets);
+        return map;
+
+      };
+
+      /* var Outdoors = L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', {
+       attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+       });
+
+       var Satellite = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+       attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+       });*/
+      /*    var Streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+       });
+
+       var map = L.map('map', {
+       center: [50.6234, 26.2189],
+       zoom: 14
+       });
+
+       map.addLayer(Streets);
+       $("#Streets span").addClass('glyphicon glyphicon-ok');*/
 
       /*$rootScope.map.locate({setView: true, maxZoom: 14});
 
-      function onLocationFound(e) {
+       function onLocationFound(e) {
 
-          L.marker(e.latlng).addTo($rootScope.map)
-              .bindPopup("You are here").openPopup();
+       L.marker(e.latlng).addTo($rootScope.map)
+       .bindPopup("You are here").openPopup();
 
-          $rootScope.userLocationArea = L.circle(e.latlng, 3000).addTo($rootScope.map);
-      }
+       $rootScope.userLocationArea = L.circle(e.latlng, 3000).addTo($rootScope.map);
+       }
 
-      $rootScope.map.on('locationfound', onLocationFound);*/
-    }
-  });
+       $rootScope.map.on('locationfound', onLocationFound);*/
+      return mapFactory;
+    });
