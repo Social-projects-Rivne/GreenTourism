3c5425c697f2c02680692eff5c8ad7d9dfcd25fa
Maxim Yushchuk <max.yushchuk@gmail.com>
2016-07-07 01:12:44 +0300

    New Welcome-Page

--- app/components/welcome-page/welcome-page.component.js
+++ app/components/welcome-page/welcome-page.component.js
@@ -2,5 +2,14 @@ angular.module('welcomePage', []);
 
 angular.module('welcomePage').component('welcomePage', {
   templateUrl: 'components/welcome-page/welcome-page.template.html',
-  controller: function() {}
+  controller: function() {
+    var $page = $('html, body');
+    $('a[href="#content"]').click(function() {
+      var heightScroll = $($.attr(this, 'href')).offset().top - 50;
+      $page.animate({
+        scrollTop: heightScroll
+      }, 1000);
+      return false;
+    });
+  }
 });
