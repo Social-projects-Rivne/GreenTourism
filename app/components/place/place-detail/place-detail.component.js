b83d124c3943f712ed0d194f6c4278d5ade5a2a0
helenrivne <martyn4uk@gmail.com>
2016-07-07 16:03:31 +0300

    Update place-detail.component.js

--- app/components/place/place-detail/place-detail.component.js
+++ app/components/place/place-detail/place-detail.component.js
@@ -5,22 +5,22 @@ angular.module('placeDetail', [])
       place: '<'
     },
     controller: function placeDetailCtrl(mapFactory) {
-        map1 = L.map('map1', {
+        map = L.map('map1', {
         center: [50.6234, 26.2189],
         zoom: 14
         });
         Streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         });
-        this.map1.addLayer(Streets);
+        this.map.addLayer(Streets);
         this.location = this.place.getLocation();
-        this.marker = L.marker(this.location).addTo(this.map1);
+        this.marker = L.marker(this.location).addTo(this.map);
 
         this.marker.bindPopup('<div><h3>' + this.place.name +
         '</h3><a><img class="marker-image" src="assets/' + this.place.photo[0] +
         '" /></a><br />').openPopup();
 
-        this.map1.setView(this.location);
+        this.map.setView(this.location);
     }
   });
 
