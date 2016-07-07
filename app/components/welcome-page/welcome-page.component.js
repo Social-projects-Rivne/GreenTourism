f2705eeb9fc6131679e56d44dff59bc810ee3316
Maxim Yushchuk <max.yushchuk@gmail.com>
2016-07-07 11:14:54 +0300

    Add Popular places on template

--- app/components/welcome-page/welcome-page.component.js
+++ app/components/welcome-page/welcome-page.component.js
@@ -1,15 +1,30 @@
-angular.module('welcomePage', []);
+angular.module('welcomePage', [])
+  .component('welcomePage', {
+    templateUrl: 'components/welcome-page/welcome-page.template.html',
+    controller: function (Place) {
+      var places = [];
+      var placeObject = {};
+      var arrPlaces = [];
+      Place.getList({_id: '577814343d5b352b9b000b59'}).then(function(result) {
+        places = result;
 
-angular.module('welcomePage').component('welcomePage', {
-  templateUrl: 'components/welcome-page/welcome-page.template.html',
-  controller: function() {
-    var page = angular.element('html, body');
-    angular.element('a[href="#main-page-content"]').click(function() {
-      var heightScroll = page.outerHeight();
-      page.animate({
-        scrollTop: heightScroll
-      }, 1000);
-      return false;
-    });
-  }
-});
+        for (var i = 0; i < places.length; i++) {
+          placeObject = {_id: places[i]._id, latitude: places[i].latitude,
+            longitude: places[i].longitude, type: places[i].type, rate: places[i].rate};
+
+          arrPlaces.push(placeObject);
+        }
+  console.log(arrPlaces);
+      });
+
+      // Animation on click arrow
+      var page = angular.element('html, body');
+      angular.element('a[href="#main-page-content"]').click(function () {
+        var heightScroll = page.outerHeight();
+        page.animate({
+          scrollTop: heightScroll
+        }, 1000);
+        return false;
+      });
+    }
+  });
