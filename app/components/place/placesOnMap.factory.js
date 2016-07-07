4ce5efb81f3620c3939d994e3294f8fd2052b59c
helenrivne <martyn4uk@gmail.com>
2016-07-07 15:29:26 +0300

    Update placesOnMap.factory.js

--- app/components/place/placesOnMap.factory.js
+++ app/components/place/placesOnMap.factory.js
@@ -47,7 +47,7 @@ angular.module('mapModule')
               marker(places[j].latitude, places[j].longitude, types[i].icon)
                   .addTo(groups[i])
                   .bindPopup("<div class='popup  center-block'><h3>" + places[j].name + "</h3><a><img class='marker-image' src='assets/" + places[j].photo + "' \/></a>" +
-                      "<br /><br /><button type='button' class='btn btn-default btn-md center-block'> <a href='#/places/"+places[j].id+"'>Details >></a> </button></div>", {autoPan:false})
+                      "<br /><br /><button type='button' class='btn btn-default btn-md center-block'> <a href='#!/places/"+places[j].id+"'>Details >></a> </button></div>", {autoPan:false})
                   .openPopup();
 
             }
@@ -59,7 +59,7 @@ angular.module('mapModule')
             marker(places[j].latitude, places[j].longitude, types[i].icon)
               .addTo(groups[i])
                 .bindPopup("<div class='popup  center-block'><h3>" + places[j].name + "</h3><a><img class='marker-image' src='assets/" + places[j].photo + "' \/></a>" +
-                    "<br /><br /><button type='button' class='btn btn-default btn-md center-block'> <a href='#/places/"+places[j].id+"'>Details >></a> </button></div>", {autoPan:false})
+                    "<br /><br /><button type='button' class='btn btn-default btn-md center-block'> <a href='#!/places/"+places[j].id+"'>Details >></a> </button></div>", {autoPan:false})
                 .openPopup();
           }
         }
