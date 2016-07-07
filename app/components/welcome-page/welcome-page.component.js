225a78616e8bcdc7e0b1ce31dd6af1dbd4febad8
Maxim Yushchuk <max.yushchuk@gmail.com>
2016-07-08 02:07:31 +0300

    Refactoring welcome-page template

--- app/components/welcome-page/welcome-page.component.js
+++ app/components/welcome-page/welcome-page.component.js
@@ -2,24 +2,9 @@ angular.module('welcomePage', [])
   .component('welcomePage', {
     templateUrl: 'components/welcome-page/welcome-page.template.html',
     controller: function (Place) {
-      var places = [];
-      var placeObject = {};
-      var arrPlaces = [];
-      Place.getList({_id: '577814343d5b352b9b000b59'}).then(function(result) {
-        places = result;
-
-        for (var i = 0; i < places.length; i++) {
-          placeObject = {_id: places[i]._id, latitude: places[i].latitude,
-            longitude: places[i].longitude, type: places[i].type, rate: places[i].rate};
-
-          arrPlaces.push(placeObject);
-        }
-  console.log(arrPlaces);
-      });
-
       // Animation on click arrow
       var page = angular.element('html, body');
-      angular.element('a[href="#main-page-content"]').click(function () {
+      angular.element('a[href="#welcome-page-content"]').click(function () {
         var heightScroll = page.outerHeight();
         page.animate({
           scrollTop: heightScroll
