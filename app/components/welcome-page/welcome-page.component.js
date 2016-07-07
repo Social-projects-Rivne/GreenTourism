ad20a4820522a3eaff754ab069cfd2a34fef147d
Maxim Yushchuk <max.yushchuk@gmail.com>
2016-07-07 09:51:58 +0300

    Fix after review

--- app/components/welcome-page/welcome-page.component.js
+++ app/components/welcome-page/welcome-page.component.js
@@ -3,10 +3,10 @@ angular.module('welcomePage', []);
 angular.module('welcomePage').component('welcomePage', {
   templateUrl: 'components/welcome-page/welcome-page.template.html',
   controller: function() {
-    var $page = $('html, body');
-    $('a[href="#content"]').click(function() {
-      var heightScroll = $($.attr(this, 'href')).offset().top - 50;
-      $page.animate({
+    var page = angular.element('html, body');
+    angular.element('a[href="#main-page-content"]').click(function() {
+      var heightScroll = page.outerHeight();
+      page.animate({
         scrollTop: heightScroll
       }, 1000);
       return false;
