3c49ff05d8604eec202500b1448a2dbffc4e90ea
Maxim Yushchuk <max.yushchuk@gmail.com>
2016-07-08 11:56:43 +0300

    Revert "Add popular place'"
    This reverts commit 1fa07af77005a9d5815862f0ced49fd73dce8bf5.

--- client/components/welcome-page/welcome-page.component.js
+++ client/components/welcome-page/welcome-page.component.js
@@ -2,12 +2,6 @@ angular.module('welcomePage', [])
   .component('welcomePage', {
     templateUrl: 'components/welcome-page/welcome-page.template.html',
     controller: function (Place) {
-      var self = this;
-
-      Place.getList({sort: 'rate', skip: 18}).then(function(result) {
-        self.popplace = result;
-      });
-
       // Animation on click arrow
       var page = angular.element('html, body');
       angular.element('a[href="#welcome-page-content"]').click(function () {
