345648d366692213b4e155abfe1cba88ac303d47
Yury Kozhenov <yurykozhenov@gmail.com>
2016-07-08 05:02:39 +0300

    Add gravatars

--- client/components/user-profile/user-profile.component.js
+++ client/components/user-profile/user-profile.component.js
@@ -5,6 +5,11 @@ angular.module('userProfile').component('userProfile', {
   controller: ['User', '$rootScope', function(User, $rootScope) {
     console.log($rootScope.globals.currentUser);
     this.user = $rootScope.globals.currentUser;
+    var gravatar = '//s.gravatar.com/avatar/' +
+                    md5(this.user.email.trim().toLowerCase()) +
+                    '?s=480';
+
+    this.user.avatar = this.user.avatar || gravatar;
 
     this.tab = 1;
 
