e324c9b426fd16bb49b6e0fd62ca2ecfd3d37098
Denys Gudakov <denysgud@gmail.com>
2016-07-10 21:24:35 +0300

    Add identification for user location

--- client/shared/map/map.factory.js
+++ client/shared/map/map.factory.js
@@ -2,25 +2,41 @@ angular.module('mapModule', [])
   .factory('mapFactory', function() {
     var mapFactory = {};
     var map;
+    var userLocation;
+
     mapFactory.showMap = function() {
       map = L.map('map', {
         center: [50.6234, 26.2189],
         zoom: 14
       });
+
+      if (!userLocation) {
+        map.locate({setView: true, maxZoom: 14});
+      } else {
+        locationArea(userLocation);
+      }
+
+      function locationArea(coords) {
+        L.circle(coords, 3000).addTo(map);
+        if (userLocation) {
+          map.setView(coords, 14);
+          console.log("yes");
+        }
+      }
+
+      function onLocationFound(e) {
+
+        var marker = L.marker(e.latlng).addTo(map)
+                .bindPopup('You are here');
+        marker.openPopup();
+        locationArea(e.latlng);
+        userLocation = e.latlng;
+      }
+
+      map.on('locationfound', onLocationFound);
       mapFactory.map = map;
       return map;
     };
 
-      /* $rootScope.map.locate({setView: true, maxZoom: 14});
-
-      function onLocationFound(e) {
-
-          L.marker(e.latlng).addTo($rootScope.map)
-              .bindPopup("You are here").openPopup();
-
-          $rootScope.userLocationArea = L.circle(e.latlng, 3000).addTo($rootScope.map);
-      }
-
-      $rootScope.map.on('locationfound', onLocationFound);*/
     return mapFactory;
   });
