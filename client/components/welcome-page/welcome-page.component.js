b4bc66ef4a48a92b681e7e6da18fed943e5e0b6a
Maxim Yushchuk <max.yushchuk@gmail.com>
2016-07-12 10:23:40 +0300

    Another fix styles after review

--- client/components/welcome-page/welcome-page.component.js
+++ client/components/welcome-page/welcome-page.component.js
@@ -9,7 +9,7 @@ angular.module('welcomePage', [])
       var i = 0;
       self.outputPopularPlaces = [];
 
-      Place.getList({sort: '-rate', limit: 10}).then(function(result) {
+      Place.getList({sort: '-rate', limit: 10}).then(function (result) {
         self.popularPlaces = result;
 
         while (i < 2) {
@@ -29,13 +29,13 @@ angular.module('welcomePage', [])
           }
           existRandom = random;
         }
-      }).then(function() {
-          Restangular.oneUrl('location', 'http://nominatim.openstreetmap.org/reverse?format=json&lat=' + self.outputPopularPlaces[0].latitude +
-            '&lon=' + self.outputPopularPlaces[0].longitude + '&addressdetails=0').get().then(function (result) {
-            self.outputPopularPlaces[0].location = result.display_name;
-          });
+      }).then(function () {
+        Restangular.oneUrl('location', 'http://nominatim.openstreetmap.org/reverse?format=json&lat=' + self.outputPopularPlaces[0].latitude +
+          '&lon=' + self.outputPopularPlaces[0].longitude + '&addressdetails=0&zoom=10').get().then(function (result) {
+          self.outputPopularPlaces[0].location = result.display_name;
+        });
         Restangular.oneUrl('location', 'http://nominatim.openstreetmap.org/reverse?format=json&lat=' + self.outputPopularPlaces[1].latitude +
-          '&lon=' + self.outputPopularPlaces[1].longitude + '&addressdetails=0').get().then(function(result) {
+          '&lon=' + self.outputPopularPlaces[1].longitude + '&addressdetails=0&zoom=10').get().then(function (result) {
           self.outputPopularPlaces[1].location = result.display_name;
         });
       });
@@ -50,4 +50,5 @@ angular.module('welcomePage', [])
         return false;
       });
     }
-  ]});
+    ]
+  });
