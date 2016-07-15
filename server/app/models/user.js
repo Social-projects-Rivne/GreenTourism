ca109082e09bef55b7895b9ed8578da835e3ffcb
Yury Kozhenov <yurykozhenov@gmail.com>
2016-07-15 04:29:43 +0300

    Update user schema

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -4,12 +4,20 @@ var bcrypt = require('bcrypt-nodejs');
 var SALT_ROUNDS = 5;
 
 var UserSchema = new Schema({
+  role: {
+    type: String,
+    lowercase: true,
+    enum: ['user', 'manager', 'admin'],
+    default: 'user',
+    required: true
+  },
   email: {
     type: String,
     unique: 'E-mail must be unique',
     required: 'E-mail is required',
     // TODO: E-mail should not have special characters (except -, _, .), replace dots
-    match: [/.+@.+\..+/, 'Please fill a valid e-mail address']
+    match: [/.+@.+\..+/, 'Please fill a valid e-mail address'],
+    trim: true
   },
   password: {
     type: String,
@@ -27,6 +35,17 @@ var UserSchema = new Schema({
   lastName: {
     type: String,
     required: 'Last name is required'
+  },
+  nickname: {
+    type: String
+  },
+  phone: {
+    type: String,
+    match: [/\+\d{1,4}\d{9}/, 'Invalid phone number'],
+    trim: true
+  },
+  address: {
+    type: String
   },
   avatar: {
     type: String
