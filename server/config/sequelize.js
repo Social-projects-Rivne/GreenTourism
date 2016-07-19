fe422aa865a96e1545e54b1ce4f5b7d20c0ab7c4
Sergii Semenkiv <semenkiv_s@mail.ru>
2016-07-20 00:11:17 +0300

    created server configuration

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -1,18 +1,21 @@
+var config = require('./config');
 var Sequelize = require('sequelize');
-var db = new Sequelize('test_greentourism', 'root', '1234', {
-  host: 'localhost',
-  dialect: 'mysql',
 
-  define: {
-    timestamps: true
-  }
-});
+module.exports = function() {
+  var sql = new Sequelize(config.sql.database,config.sql.username, config.sql.password, {
+    host: config.sql.host,
+    dialect: config.sql.dialect,
+    define: {
+      timestamps: true
+    }
+  });
 
 // Test connection
-db.authenticate().then(function() {
-  console.log('Connection has been established successfully.');
-}).catch(function(err) {
-  console.log('Unable to connect to the database:', err);
-});
+  sql.authenticate().then(function() {
+    console.log('Connection has been established successfully.');
+  }).catch(function(err) {
+    console.log('Unable to connect to the database:', err);
+  });
 
-module.exports = db;
+  return sql;
+};
