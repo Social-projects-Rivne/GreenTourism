fe11d414276ed4e42b2caf4b4d362ee6e9d7715f
Yury Kozhenov <yurykozhenov@gmail.com>
2016-07-19 16:26:42 +0300

    Update auth.component.js

--- client/components/user/auth/auth/auth.component.js
+++ client/components/user/auth/auth/auth.component.js
@@ -1,7 +1,7 @@
 angular.module('user').component('auth', {
   templateUrl: 'components/user/auth/auth/auth.template.html',
   controller: [function authCtrl() {
-  	this.form = 'login';
+    this.form = 'login';
 
     angular.element('.dropdown-menu').on({
       click: function(e) {
