81c40e18fa1ad63b31197e0128a7e94f1377319a
TarasRudyk <rudyktaras@gmail.com>
2016-07-22 20:29:36 +0300

    signup is ready

--- client/components/user/auth/signup/signup.component.js
+++ client/components/user/auth/signup/signup.component.js
@@ -1,52 +1,52 @@
 angular.module('user').component('signup', {
   templateUrl: 'components/user/auth/signup/signup.template.html',
   controller: [function signupCtrl() {
-    this.signup = function(isvalid) {
-      if (!isvalid) {
-        this.showError = true;
-      }
-    };
+    // this.signup = function(isvalid) {
+    //   if (!isvalid) {
+    //     this.showError = true;
+    //   }
+    // };
 
-    this.getErrorpassword = function(error) {
-      if (angular.isDefined(error)) {
-        if (error.pattern) {
-          return '1 Alphabet, 1 Number and 1 Special Character';
-        } else if (error.maxlength) {
-          return 'Sorry, but less then 25 characters is allowed';
-        } else if (error.minlength) {
-          return 'Please input more then 7 characters';
-        } else if (error.match) {
-          return 'Password isn\'t match';
-        } else if (error.required) {
-          return 'Please fill up this field';
-        }
-      }
-    };
+    // this.getErrorpassword = function(error) {
+    //   if (angular.isDefined(error)) {
+    //     if (error.pattern) {
+    //       return '1 Alphabet, 1 Number and 1 Special Character';
+    //     } else if (error.maxlength) {
+    //       return 'Sorry, but less then 25 characters is allowed';
+    //     } else if (error.minlength) {
+    //       return 'Please input more then 7 characters';
+    //     } else if (error.match) {
+    //       return 'Password isn\'t match';
+    //     } else if (error.required) {
+    //       return 'Please fill up this field';
+    //     }
+    //   }
+    // };
 
-    this.getErroremail = function(error) {
-      if (angular.isDefined(error)) {
-        if (error.required) {
-          return 'Please fill up this field';
-        } else if (error.email) {
-          return 'Please input correct email';
-        } else if (error.pattern) {
-          return 'Only top level domein is allowed';
-        }
-      }
-    };
+    // this.getErroremail = function(error) {
+    //   if (angular.isDefined(error)) {
+    //     if (error.required) {
+    //       return 'Please fill up this field';
+    //     } else if (error.email) {
+    //       return 'Please input correct email';
+    //     } else if (error.pattern) {
+    //       return 'Only top level domein is allowed';
+    //     }
+    //   }
+    // };
 
-    this.getError = function(error) {
-      if (angular.isDefined(error)) {
-        if (error.required) {
-          return 'Please fill up this field';
-        } else if (error.minlength) {
-          return 'Please input more then 2 characters';
-        } else if (error.pattern) {
-          return 'Only alphabet is required';
-        } else if (error.maxlength) {
-          return 'Sorry, but less then 15 characters is allowed';
-        }
-      }
-    };
+    // this.getError = function(error) {
+    //   if (angular.isDefined(error)) {
+    //     if (error.required) {
+    //       return 'Please fill up this field';
+    //     } else if (error.minlength) {
+    //       return 'Please input more then 2 characters';
+    //     } else if (error.pattern) {
+    //       return 'Only alphabet is required';
+    //     } else if (error.maxlength) {
+    //       return 'Sorry, but less then 15 characters is allowed';
+    //     }
+    //   }
+    // };
   }]
 });
