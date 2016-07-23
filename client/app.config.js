a385506ab3a773a2e30454e833eb8060442a2a64
helenrivne <martyn4uk@gmail.com>
2016-07-23 21:19:29 +0300

    Correct some code

--- client/app.config.js
+++ client/app.config.js
@@ -5,23 +5,3 @@ angular.module('greenTourism')
       id: '_id'
     });
   }]);
-
-angular.module('greenTourism')
-  .controller('MainCtrl', ['$rootScope', '$scope', function MainCtrl($rootScope, $scope) {
-    var ctrl = this;
-    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
-      ctrl.routeChangeError = false;
-      ctrl.statusCode = 200;
-      $scope.pageClass = 'page page-detail';
-      $scope.$on('closePage', function(event, data) {
-        $scope.pageClass = 'page page-detail ng-leave';
-        location.href = "#!/places";
-      });
-    });
-
-    $rootScope.$on('$routeChangeError', function(event, current, previous,
-                                                 rejection) {
-      ctrl.routeChangeError = true;
-      ctrl.statusCode = rejection.status;
-    });
-  }]);
--- client/shared/main.controller.js
+++ client/shared/main.controller.js
@@ -4,9 +4,14 @@ angular.module('greenTourism')
 
     // TODO: Add flash messages hiding when changing route
 
-    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+    $rootScope.$on('$routeChangeSuccess', '$scope', function(event, current, previous, $scope) {
       ctrl.routeChangeError = false;
       ctrl.statusCode = 200;
+      $scope.pageClass = 'page-detail';
+      $scope.$on('closePage', function(event, data) {
+        $scope.pageClass = 'page-detail ng-leave';
+        location.href = "#!/places";
+      });
     });
 
     $rootScope.$on('$routeChangeError', function(event, current, previous,
