11b2656565a312d573c0f44cfc382aa9939ec5d5
helenrivne <martyn4uk@gmail.com>
2016-07-23 10:28:30 +0300

    Add images responsibility disable scrolling

--- client/components/place/place-detail/place-detail.component.js
+++ client/components/place/place-detail/place-detail.component.js
@@ -16,15 +16,18 @@ angular.module('placeDetail', [])
         center: constants.mapCenter,
         zoom: constants.defaultZoom
       });
-      Streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      this.map.touchZoom.disable();
+      this.map.dragging.disable();
+      this.map.scrollWheelZoom.disable();
+      var layerStreet = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       });
-      this.map.addLayer(Streets);
+      this.map.addLayer(layerStreet);
       this.marker = L.marker(L.latLng(this.place.location.coordinates[0], this.place.location.coordinates[1])).addTo(this.map);
       this.marker.bindPopup('<div class="popup"><h3>' + this.place.name +
-        '</h3><a><img class="marker-image center-block" src="'+this.place.photos[0]+'" /></a><br />').openPopup();
-
-      this.map.setView([this.place.location.coordinates[0]+0.005, this.place.location.coordinates[1]]); //0.005- for responcive design- show info on mobile
+        '</h3><a><img class="marker-image center-block" src="' + this.place.photos[0] + '" /></a><br />').openPopup();
+      var deltaheight = 0.005;
+      this.map.setView([this.place.location.coordinates[0] + deltaheight, this.place.location.coordinates[1]]); //0.005- for responcive design- show info on mobile
       this.closePage = function() {
         $scope.$emit('closePage', 'pageClass');
 
