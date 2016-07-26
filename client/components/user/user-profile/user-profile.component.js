17a1dbb53573c884a8ce046b4240af44d420e1ae
Yury Kozhenov <yurykozhenov@gmail.com>
2016-07-27 02:07:25 +0300

    Add change avatar form

--- client/components/user/user-profile/user-profile.component.js
+++ client/components/user/user-profile/user-profile.component.js
@@ -16,11 +16,18 @@ angular.module('user').component('userProfile', {
 
     // User editing
     ctrl.editMode = false;
+    ctrl.editAvatarMode = false;
+
+    ctrl.toggleEditAvatarMode = function() {
+      ctrl.editAvatarMode = !ctrl.editAvatarMode;
+      ctrl.newAvatar = null;
+    };
 
     ctrl.changeAvatar = function() {
-      // TODO: Replace prompt with something different
-      ctrl.user.avatar = prompt('Enter new avatar url');
+      ctrl.user.avatar = ctrl.newAvatar;
       ctrl.user.save();
+
+      ctrl.toggleEditAvatarMode();
     };
 
     ctrl.toggleEditMode = function() {
@@ -29,15 +36,13 @@ angular.module('user').component('userProfile', {
       if (ctrl.editMode) {
         ctrl.editedUser = angular.copy(ctrl.user);
       } else {
-        delete ctrl.editedUser;
+        ctrl.editedUser = null;
       }
     };
 
     ctrl.editUser = function() {
-      // TODO: Validate before saving
       angular.merge(ctrl.user, ctrl.editedUser);
       ctrl.user.save();
-      delete ctrl.user.password;  // Password must not be stored in app
 
       ctrl.toggleEditMode();
     };
