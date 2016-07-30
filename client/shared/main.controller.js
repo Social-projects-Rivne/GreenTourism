f5101c63c1e9064f88fab881a7bc7b0604d9c2eb
Yury Kozhenov <yurykozhenov@gmail.com>
2016-07-30 22:49:44 +0300

    Refactor place detail animation logic

--- client/shared/main.controller.js
+++ client/shared/main.controller.js
@@ -1,25 +1,36 @@
 angular.module('greenTourism')
-  .controller('MainCtrl', ['$rootScope', '$scope', '$location', function MainCtrl($rootScope, $scope, $location) {
-    var ctrl = this;
+  .controller('MainCtrl', ['$rootScope', '$location',
+    function MainCtrl($rootScope, $location) {
+      var ctrl = this;
 
-    if ($location.path().split('/').slice(1, 2).join('/') != "places") {
-      $scope.pageClass = 'page';
+      // TODO: Add flash messages hiding when changing route
+
+      $rootScope.$on('$routeChangeSuccess',
+        function(event, current, previous) {
+          var placeDetailRoute = /^\/places\/\.*/;
+
+          var isPlaceDetailCurrentRoute = placeDetailRoute.test(
+            current.$$route.originalPath);
+          var isPlaceDetailPreviousRoute = previous &&
+            placeDetailRoute.test(previous.$$route.originalPath);
+
+          // Check if current or previous route matching place detail page
+          if (isPlaceDetailCurrentRoute || isPlaceDetailPreviousRoute) {
+            angular.element('#js-router').addClass('page-detail');
+          } else {
+            angular.element('#js-router').removeClass('page-detail');
+          }
+
+          ctrl.routeChangeError = false;
+          ctrl.statusCode = 200;
+        }
+      );
+
+      $rootScope.$on('$routeChangeError',
+        function(event, current, previous, rejection) {
+          ctrl.routeChangeError = true;
+          ctrl.statusCode = rejection.status;
+        }
+      );
     }
-    $scope.$on('closePage', function(event, data) {
-      location.href = "#!/places";
-    });
-
-    // TODO: Add flash messages hiding when changing route
-
-    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
-
-      ctrl.routeChangeError = false;
-      ctrl.statusCode = 200;
-    });
-
-    $rootScope.$on('$routeChangeError', function(event, current, previous,
-                                                 rejection) {
-      ctrl.routeChangeError = true;
-      ctrl.statusCode = rejection.status;
-    });
-  }]);
+  ]);
