b235572350560c3af79a855064d5dde71d003bda
Maxim Yushchuk <max.yushchuk@gmail.com>
2016-07-30 20:58:14 +0300

    Remove MAX_COMMENT_CONTENT_LENGTH

--- client/shared/comment/comment.component.js
+++ client/shared/comment/comment.component.js
@@ -8,44 +8,37 @@ angular.module('comment', [])
     controller: ['currentUser', 'Restangular',
       function(currentUser, Restangular) {
         var ctrl = this;
-        var MAX_COMMENT_CONTENT_LENGTH = 1000;
 
         ctrl.currentUser = currentUser;
         ctrl.showError = false;
         ctrl.addComment = function(content) {
-          if (content.length <= MAX_COMMENT_CONTENT_LENGTH) {
+          Restangular.one(ctrl.inputObjectType + '/' +
+            ctrl.inputObject._id + '/comments').customPOST(
+            {
+              content: content,
+              author: ctrl.currentUser._id
+            }).then(function(res) {
+            ctrl.content = '';
             Restangular.one(ctrl.inputObjectType + '/' +
-              ctrl.inputObject._id + '/comments').customPOST(
-              {
-                content: content,
-                author: ctrl.currentUser._id
-              }).then(
-                function(res) {
-                  ctrl.content = '';
-                  Restangular.one(ctrl.inputObjectType + '/' +
-                    ctrl.inputObject._id + '/comments/' + res)
-                  .get().then(function(obj) {
-                    ctrl.inputObject.comments.push(obj);
-                  });
-                },
-                function(err) {
-                  ctrl.showError = err.statusText;
-                });
-          }
+              ctrl.inputObject._id + '/comments/' + res)
+              .get().then(function(obj) {
+              ctrl.inputObject.comments.push(obj);
+            });
+          }, function(err) {
+            ctrl.showError = err.statusText;
+          });
         };
 
         ctrl.removeComment = function(id) {
           Restangular.one(ctrl.inputObjectType + '/' +
-            ctrl.inputObject._id + '/comments', id).remove().then(
-              function() {
-                ctrl.inputObject.comments = ctrl.inputObject.comments
-                .filter(function(comment) {
-                  return comment._id !== id;
-                });
-              },
-              function(err) {
-                ctrl.showError = err.statusText;
+            ctrl.inputObject._id + '/comments', id).remove().then(function() {
+            ctrl.inputObject.comments = ctrl.inputObject.comments
+              .filter(function(comment) {
+                return comment._id !== id;
               });
+          }, function(err) {
+            ctrl.showError = err.statusText;
+          });
         };
 
         ctrl.showEditingMode = function(id, defaultContent) {
@@ -54,16 +47,14 @@ angular.module('comment', [])
         };
 
         ctrl.updateComment = function(id, content) {
-          if (content.length <= MAX_COMMENT_CONTENT_LENGTH) {
-            Restangular.one(ctrl.inputObjectType + '/' +
-              ctrl.inputObject._id + '/comments', id)
-              .get().then(
-                function(obj) {
-                  obj.content = content;
-                  obj.put();
-                  ctrl.checkCommentId = null;
-                });
-          }
+          Restangular.one(ctrl.inputObjectType + '/' +
+            ctrl.inputObject._id + '/comments', id)
+            .get().then(
+            function(obj) {
+              obj.content = content;
+              obj.put();
+              ctrl.checkCommentId = null;
+            });
         };
       }
     ]
