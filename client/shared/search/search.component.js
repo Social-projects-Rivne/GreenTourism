7d5a56dab02e29836be99f0af81d56d90da58951
helenrivne <martyn4uk@gmail.com>
2016-07-31 00:38:23 +0300

    Change search component

--- client/shared/search/search.component.js
+++ client/shared/search/search.component.js
@@ -1,13 +1,33 @@
-angular.module('searchPlace', ['ui.bootstrap','ngAnimate'])
+angular.module('greenTourism',['ui.bootstrap'])
   .component('searchPlace', {
     templateUrl: 'shared/search/search.template.html',
     controller: ['Search', '$scope', function SearchCtrl(Search, $scope) {
+      var minLength = 3;
+      $scope.loading = false;
       var searchBy = 'place';
-      $scope.noResults = false;
-      $scope.placesearch = '';
-      Search.getList({ searchBy: [searchBy]}).then(function(result) {
-        $scope.places=result;
-        console.log( $scope.places);
-      });
+      $scope.delay = 500;
+
+      $scope.onSelect = function(place){
+        $scope.selected = place;
+      };
+      $scope.places = function(placeName) {
+        if (placeName.length < minLength) {
+          return [];
+        }
+        $scope.loading = true;
+        return Search.getList({name: [searchname], searchBy: [searchBy]}).then(function(result) {
+          $scope.loading = false;
+          return result;
+        }, function(status){
+          $scope.loading = false;
+        });
+      };
+      /*
+       this.searchPlace = function(searchname) {
+       Search.getList({name: [searchname], searchBy: [searchBy]}).then(function(result) {
+       console.log("SEARCH");
+       console.log(result);
+       });
+       }*/
     }]
   });
--- server/app/controllers/search.js
+++ server/app/controllers/search.js
@@ -1,16 +1,15 @@
 var Search = require('mongoose').model('Place');
 
 exports.list = function(req, res) {
-
-  //var name= req.query.name;
- // var regex = new RegExp(req.query.name, 'i');
- // delete req.query.name;
+  var name= req.query.name;
+  var regex = new RegExp(req.query.name, 'i');
+  delete req.query.name;
 
   var searchBy = req.query.searchBy;
   delete req.query.searchBy;
 
   if (searchBy == 'place') {
-    Search.find({},'name',
+    Search.find({regex},'name',
       function(err, records) {
         if (err) {
           res.status(400).json(err);
