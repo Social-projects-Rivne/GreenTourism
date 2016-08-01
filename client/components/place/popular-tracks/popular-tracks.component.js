b891160f97672326d2b2587c1f4ee894ad01334b
Yury Kozhenov <yurykozhenov@gmail.com>
2016-08-01 22:59:33 +0300

    Update popular-tracks.component.js

--- client/components/place/popular-tracks/popular-tracks.component.js
+++ client/components/place/popular-tracks/popular-tracks.component.js
@@ -5,7 +5,7 @@ angular.module('popularTracks', [])
       var self = this;
       self.popularTracks = [];
       var userLocation;
-      
+
       if (mapFactory.popularTracks) {
         self.popularTracks = mapFactory.popularTracks;
       } else {
