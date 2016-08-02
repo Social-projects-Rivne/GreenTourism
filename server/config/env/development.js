af480a2b34ff81a68a3f4222551ec8ef0c91d095
Yury Kozhenov <yurykozhenov@gmail.com>
2016-08-02 15:42:15 +0300

    Change oauth callbacks

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -14,11 +14,11 @@ module.exports = {
   facebook: {
     clientID: '1626051384389779',
     clientSecret: '6d0b876c83e0edc2e55748a3709e36f0',
-    callbackURL: 'http://localhost:8080/api/auth/facebook/callback'
+    callbackURL: 'https://green-tourism.herokuapp.com/api/auth/facebook/callback'
   },
   google: {
     clientID: '54757690485-qi1sur7efphqrii06aoafdon990k1vr0.apps.googleusercontent.com',
     clientSecret: 'Bk0dYxY_fU6GIk1UBxxEIcSz',
-    callbackURL: 'http://localhost:8080/api/auth/google/callback'
+    callbackURL: 'https://green-tourism.herokuapp.com/api/auth/facebook/callback'
   }
 };
