8ab4e6725b507caca3fa6323aa8ff9fef7e4e142
Yury Kozhenov <yurykozhenov@gmail.com>
2016-08-03 00:21:16 +0300

    Fix animation working on welcome page

--- client/shared/main.controller.js
+++ client/shared/main.controller.js
@@ -1,21 +1,24 @@
 angular.module('greenTourism')
-  .controller('MainCtrl', ['$rootScope', '$location',
-    function MainCtrl($rootScope, $location) {
+  .controller('MainCtrl', ['$rootScope', '$location', '$window',
+    function MainCtrl($rootScope, $location, $window) {
       var ctrl = this;
 
       // TODO: Add flash messages hiding when changing route
 
       $rootScope.$on('$routeChangeSuccess',
         function(event, current, previous) {
+          $window.scrollTo(0, 0);
+
           var placeDetailRoute = /^\/places\/\.*/;
+          var placeRoute = /^\/places/;
 
           var isPlaceDetailCurrentRoute = placeDetailRoute.test(
             current.$$route.originalPath);
-          var isPlaceDetailPreviousRoute = previous &&
-            placeDetailRoute.test(previous.$$route.originalPath);
+          var isPlacePreviousRoute = previous &&
+            placeRoute.test(previous.$$route.originalPath);
 
-          // Check if current or previous route matching place detail page
-          if (isPlaceDetailCurrentRoute || isPlaceDetailPreviousRoute) {
+          // Check if current route matching place detail page
+          if (isPlaceDetailCurrentRoute && isPlacePreviousRoute) {
             ctrl.placeDetailPage = true;
           } else {
             ctrl.placeDetailPage = false;
