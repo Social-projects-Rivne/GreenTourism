1c833a61ae784bcd2ffc8a032d4bbf637e7f7c25
Yury Kozhenov <yurykozhenov@gmail.com>
2016-08-06 02:23:44 +0300

    Update links to my places and tracks

--- client/components/user/user-profile/tab-list/tab-list.component.js
+++ client/components/user/user-profile/tab-list/tab-list.component.js
@@ -1,6 +1,7 @@
 angular.module('user').component('tabList', {
   templateUrl: 'components/user/user-profile//tab-list/tab-list.template.html',
   bindings: {
-    items: '<'
+    items: '<',
+    route: '<'
   }
 });
