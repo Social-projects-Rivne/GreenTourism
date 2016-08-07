786dfc55beca13f763682ad23684d1b515211fea
AndriyZhuk <astrosferatu@gmail.com>
2016-08-07 19:09:53 +0300

    Can add new event in db, better search events in db

---
+++ client/components/event/events-detail/event-detail.component.js
@@ -0,0 +1,21 @@
+angular.module('eventDetail').component('eventDetail', {
+  templateUrl: 'components/event/event-detail/event-detail.template.html',
+  controller: ['$scope', '$routeParams', 'calendarService', 'Event',
+    function($scope, $routeParams, calendarService, Event) {
+      this._id = $routeParams.eventId;
+
+      $scope.calendars = calendarService;
+
+      $scope.calendars.clear() ;
+
+      if ($scope.calendars.events.length == 0)
+      {
+          Event.getList().then(function (result) {
+          $scope.calendars.events = result.concat(event);
+        })
+      };
+
+      $scope.eventListService = $scope.calendars.events;
+
+   }]
+});
---
+++ client/components/event/events-detail/event-list.js
@@ -0,0 +1,80 @@
+angular.module('eventDetail', []);
+
+angular.module('eventDetail').service('eventListService', ['$http', function($http) {
+  if (!this.date_current) new Date();
+  this.CalendarName = [];
+  this.CalendarValue = [];
+  this.Type = [];
+  this.mainController = 'Noname';
+  this.mainControllerName = 'Noname';
+  this.TypeMenu = [];
+  this.itemTypeMenu;
+
+  this.event = [];
+
+  this.findDate = function(dateFind, sort) {
+    return this.event.filter(function(itEvent) {
+      var tempDate = new Date(itEvent.dateStart);
+      if (sort == 1) return tempDate > dateFind;
+      return tempDate.toDateString() == dateFind.toDateString();
+    });
+  };
+
+  this.findDistance = function(lat, lng, event) {
+    function compareEvent(eventA, eventB) {
+      var comperA = +Math.pow(Math.abs(+eventA.eventPoints[0].lat - lat), 2) + Math.pow(Math.abs(+eventA.eventPoints[0].lon - lng), 2);
+      var comperB = +Math.pow(Math.abs(+eventB.eventPoints[0].lat - lat), 2) + Math.pow(Math.abs(+eventB.eventPoints[0].lon - lng), 2);
+      return comperA - comperB;
+    }
+    return event.sort(compareEvent);
+  };
+
+  this.find_event = function(dateStart, dateEnd, type) {
+    return this.event.filter(function(itEvent) {
+      var _startDate = new Date(itEvent.dateStart);
+      var _endDate = new Date(itEvent.dateEnd);
+      var _type = itEvent.type;
+      if (_startDate >= dateStart && _endDate <= dateEnd) {
+        for (i = 0; i < type.length; i++)
+          if (_type == type[i]) return true;
+      }
+    });
+  };
+
+  this.all_type = function() {
+    for (j = 0; j < this.event.length; j++) {
+      if (this.Type.indexOf(this.event[j].type) == -1) {
+        this.Type.push(this.event[j].type);
+        var pushObj = {};
+        pushObj.active = false;
+        pushObj.type = this.event[j].type;
+        this.TypeMenu.push(pushObj);
+      }
+    }
+    return this.TypeMenu;
+  };
+
+  this.active_type = function() {
+    var types = [];
+    for (j = 0; j < this.TypeMenu.length; j++) {
+      if (this.TypeMenu[j].active) types.push(this.TypeMenu[j].type);
+    }
+    return types;
+  };
+
+  this.clear = function() {
+    this.CalendarName = [];
+    this.Type = [];
+    this.mainController = 'Noname';
+    this.mainControllerName = 'Noname';
+    this.TypeMenu = [];
+    this.itemTypeMenu;
+    this.CalendarValue = [];
+
+    return;
+  };
+
+  return {
+    th: this
+  };
+}]);
