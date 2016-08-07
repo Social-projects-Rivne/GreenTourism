063d9a7ebb36cde25fe32e6693b04827cb17b375
Denys Gudakov <denysgud@gmail.com>
2016-08-07 18:09:12 +0300

    Add link from popular tracks to track detail page and fix small bags in popular track component

--- client/components/place/constants.factory.js
+++ client/components/place/constants.factory.js
@@ -3,7 +3,7 @@ angular.module('greenTourism')
     var constants = {};
     constants.defaultZoom = 14;
     constants.defaultOpacity = 1;
-    constants.radiusForPopularItems = 5000;
+    constants.radiusForPopularItems = 50000;
     constants.radiusForAddingTrackPoints = 2000;
     constants.mapCenter = [50.6234, 26.2189];
     // For Places and Tracks
