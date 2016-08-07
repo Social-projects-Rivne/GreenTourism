5a0a926e28d9bb6a5ceff11c4aa028758faec0f7
helenrivne <martyn4uk@gmail.com>
2016-08-07 14:04:59 +0300

    Remove small bugs

--- client/components/place/place-detail/map-place.factory.js
+++
@@ -1,9 +0,0 @@
-angular.module('mapModule')
-  .factory('mapPlaceFactory', ['constants', function(constants) {
-    /*var map =  L.map('map1', {
-      center: constants.mapCenter,
-      zoom: constants.defaultZoom-8
-    });
-
-    return map;*/
-  }]);
