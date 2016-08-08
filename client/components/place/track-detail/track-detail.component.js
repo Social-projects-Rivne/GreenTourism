2f0815034373cc8ca16fb89704ac43b08098f0bf
Denys Gudakov <denysgud@gmail.com>
2016-08-08 17:21:18 +0300

    Add functionality for deleting photos

--- client/components/place/track-detail/track-detail.component.js
+++ client/components/place/track-detail/track-detail.component.js
@@ -4,11 +4,141 @@ angular.module('trackDetail', ['comment', 'like'])
     bindings: {
       track: '<'
     },
-    controller: ['$scope', 'constants', 'mapMarkingTypes', '$timeout', 'Track', 'placesOnMap', 'preloadImages',
-      function trackDetailCtrl($scope, constants, mapMarkingTypes, $timeout, Track, placesOnMap, preloadImages) {
+    controller: ['$scope', 'constants', 'mapMarkingTypes', '$timeout', 'Track',
+                 'placesOnMap', 'preloadImages', 'currentUser',
+      function trackDetailCtrl($scope, constants, mapMarkingTypes, $timeout,
+                               Track, placesOnMap, preloadImages, currentUser) {
         var ctrl = this;
+        ctrl.currentUser = currentUser;
+        ctrl.track = this.track;
+        ctrl.hideForm = true;
+        ctrl.trackTypes = mapMarkingTypes.tracks;
         var placesInTrackArray = [];
         ctrl.placesInTrack = [];
+        ctrl.currentTrackType = ctrl.track.type;
+        ctrl.defaultTrack = angular.copy(ctrl.track);
+        ctrl.newPhoto = '';
+        ctrl.fotoIsNotCovered = {};
+
+        ctrl.track.photos.forEach(function(track, index) {
+          ctrl.fotoIsNotCovered[index] = true;
+        });
+
+        ctrl.enableEditMode = function() {
+          ctrl.showMessage = false;
+          ctrl.message = '';
+          angular.element('#show-message').removeClass();
+          angular.element('.edit-button').toggleClass('edit-mode-active');
+          angular.element('#editPlaceForm').css('display', 'block');
+          ctrl.defaultTrack = angular.copy(ctrl.track);
+          ctrl.track.type = ctrl.currentTrackType;
+
+          if (ctrl.hideForm) {
+            ctrl.hideForm = false;
+          } else {
+            ctrl.hideForm = true;
+          }
+        };
+
+        ctrl.updateTrack = function(track) {
+          angular.element('#show-message').removeClass('alert-info');
+          ctrl.message = '';
+          if (track) {
+            var trackObject = {
+              name: track.name,
+              description: track.description,
+              type: (function() {
+                return track.type || ctrl.defaultTrack.type;
+              })(),
+              photos: track.photos
+            };
+
+            if (_.isEqual(trackObject.photos, ctrl.defaultTrack.photos)) {
+              delete trackObject.photos;
+            }
+
+            for (var key in trackObject) {
+              if ({}.hasOwnProperty.call(trackObject, key)) {
+                if (trackObject[key] === ctrl.defaultTrack[key])
+                  delete trackObject[key];
+              }
+            }
+            if (Object.keys(trackObject).length) {
+              Track.one(ctrl.track._id).customPUT(trackObject)
+                .then(function(obj) {
+                  ctrl.defaultTrack = angular.copy(obj.record);
+                  ctrl.track = angular.copy(ctrl.defaultTrack);
+                  angular.element('#show-message')
+                    .addClass('alert alert-success text-center');
+                  ctrl.message = 'Your changes was saved successfully!';
+                  ctrl.showMessage = true;
+                  redrawTrack();
+                }, function() {
+                  angular.element('#show-message').removeClass();
+                  angular.element('#show-message')
+                    .addClass('alert alert-danger text-center');
+                  ctrl.message = 'There is some problem. ' +
+                    'Please, reload page and try again';
+                });
+            } else {
+              ctrl.showMessage = true;
+              angular.element('#show-message')
+                .addClass('alert alert-info text-center');
+              ctrl.message = 'Nothing to change';
+            }
+          } else {
+            angular.element('#show-message').removeClass();
+            ctrl.message = '';
+            ctrl.showMessage = false;
+            ctrl.track = angular.copy(ctrl.defaultTrack);
+            ctrl.track.type = ctrl.currentTrackType;
+            ctrl.track.photos.forEach(function(track, index) {
+              ctrl.fotoIsNotCovered[index] = true;
+            });
+          }
+        };
+
+        function redrawTrack() {
+          ctrl.map.removeLayer(currenTrack);
+          var newColor = mapMarkingTypes.tracks[ctrl.track.type].color;
+          drawTrack(coordsArray, newColor);
+        }
+
+        function drawTrack(coords, color) {
+          return L.polyline(coords, {
+            color: color,
+            opacity: 1
+          }).addTo(ctrl.map);
+        }
+
+        ctrl.deletePhoto = function(index) {
+          ctrl.track.photos.splice(index, 1);
+        };
+
+        ctrl.addPhoto = function(track) {
+          if (_.includes(ctrl.track.photos, track)) {
+            ctrl.message = 'Nothing to change';
+            ctrl.showMessage = true;
+          } else {
+            ctrl.track.photos.push(ctrl.newPhoto);
+            ctrl.newPhoto = '';
+          }
+          ctrl.track.photos.forEach(function(track, index) {
+            ctrl.fotoIsNotCovered[index] = true;
+          });
+        };
+
+        ctrl.mouseOverPhoto = function(index) {
+          ctrl.track.photos.forEach(function(track, index) {
+            ctrl.fotoIsNotCovered[index] = true;
+          });
+          ctrl.fotoIsNotCovered[index] = false;
+        };
+
+        ctrl.mouseOutPhoto = function(index) {
+          ctrl.fotoIsNotCovered[index] = true;
+        };
+
         angular.element(document).ready(function() {
           angular.element('.fancybox').fancybox();
         });
@@ -34,10 +164,11 @@ angular.module('trackDetail', ['comment', 'like'])
           coords[1] = coord[0];
           coordsArray[index] = coords;
         });
-        L.polyline(coordsArray, {
+        var currenTrack = drawTrack(coordsArray, color);
+        /* L.polyline(coordsArray, {
           color: color,
           opacity: 1
-        }).addTo(ctrl.map);
+        }).addTo(ctrl.map);*/
 
         ctrl.map.fitBounds(coordsArray);
 
@@ -66,7 +197,7 @@ angular.module('trackDetail', ['comment', 'like'])
           ctrl.map.closePopup();
           ctrl.map.fitBounds(coordsArray);
         };
-        
+
         ctrl.indexBegin = 1;
         $scope.numberOfphoto = 6;
         $scope.loading = false;
