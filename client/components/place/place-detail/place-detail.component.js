6ce26b4007dfc077680c5ae27d1d848980c4b6d2
max.yushchuk <max.yushchuk@gmail.com>
2016-08-08 17:03:40 +0300

    Add functionality for editing places

--- client/components/place/place-detail/place-detail.component.js
+++ client/components/place/place-detail/place-detail.component.js
@@ -1,14 +1,103 @@
-angular.module('placeDetail', ['comment', 'like'])
+angular.module('placeDetail', ['comment', 'like', 'ngAnimate', 'ngMessages'])
   .component('placeDetail', {
     templateUrl: 'components/place/place-detail/place-detail.template.html',
     bindings: {
       place: '<'
     },
-    controller: function placeDetailCtrl($scope, constants, mapMarkingTypes, preloadImages, $timeout, Place) {
+    controller: function placeDetailCtrl($scope, constants, mapMarkingTypes, preloadImages, $timeout, Place, currentUser) {
       angular.element(document).ready(function() {
         angular.element('.fancybox').fancybox();
       });
       var ctrl = this;
+      ctrl.removePhotosArray = [];
+
+      ctrl.placesType = mapMarkingTypes.places;
+      ctrl.currentUser = currentUser;
+      ctrl.hideForm = true;
+      ctrl.defaultPlace = angular.copy(ctrl.place);
+
+      ctrl.removePhoto = function(photo) {
+        ctrl.removePhotosArray.push(photo);
+        ctrl.place.photos = _.without(ctrl.place.photos, photo);
+      };
+
+      ctrl.addPhoto = function(photo) {
+        ctrl.error = '';
+        if (photo) {
+          if (_.includes(ctrl.place.photos, photo)) {
+            ctrl.error = 'This URL is already exist. Please, paste another URL';
+          } else {
+            ctrl.place.photos.push(photo);
+            angular.element('#add-photo')[0].value = '';
+          }
+        } else {
+          ctrl.error = 'You did not attach any image URL';
+        }
+      };
+
+      ctrl.enableEditMode = function() {
+        ctrl.showMessage = false;
+        ctrl.message = '';
+        angular.element('#show-message').removeClass();
+        angular.element('.edit-button').toggleClass('edit-mode-active');
+        angular.element('#editPlaceForm').css('display', 'block');
+
+        if (ctrl.hideForm) ctrl.hideForm = false;
+        else ctrl.hideForm = true;
+      };
+
+      ctrl.updatePlace = function(place) {
+        angular.element('#show-message').removeClass('alert-info');
+        ctrl.message = '';
+        if (place) {
+          var placeObject = {
+            name: place.name,
+            description: place.description,
+            address: place.address,
+            photos: ctrl.place.photos,
+            type: (function() {
+              return place.type1 || ctrl.defaultPlace.type;
+            })()
+          };
+
+          for (var key in placeObject) {
+            if ({}.hasOwnProperty.call(placeObject, key)) {
+              if (placeObject[key] === ctrl.defaultPlace[key])
+                delete placeObject[key];
+            }
+          }
+          if (_.isEqual(placeObject.photos, ctrl.defaultPlace.photos))
+            delete placeObject.photos;
+
+          if (Object.keys(placeObject).length) {
+            Place.one(ctrl.place._id).customPUT(placeObject)
+              .then(function(obj) {
+                ctrl.showMessage = true;
+                ctrl.defaultPlace = angular.copy(obj.record);
+                ctrl.place = angular.copy(ctrl.defaultPlace);
+                angular.element('#show-message')
+                  .addClass('alert alert-success text-center');
+                ctrl.message = 'Your changes was saved successfully!';
+              }, function() {
+                angular.element('#show-message').removeClass();
+                angular.element('#show-message')
+                  .addClass('alert alert-danger text-center');
+                ctrl.message = 'There is some problem. ' +
+                  'Please, reload page and try again';
+              });
+          } else {
+            ctrl.showMessage = true;
+            angular.element('#show-message')
+              .addClass('alert alert-info text-center');
+            ctrl.message = 'Nothing to change';
+          }
+        } else {
+          angular.element('#show-message').removeClass();
+          ctrl.message = '';
+          ctrl.showMessage = false;
+          ctrl.place = angular.copy(ctrl.defaultPlace);
+        }
+      };
 
       if(ctrl.marker){
         ctrl.marker.clearLayers();
